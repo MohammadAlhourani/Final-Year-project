3d34a68f5f7369468a07b04d3613d599ce869650
MohammadAlhourani <34336016+MohammadAlhourani@users.noreply.github.com>
2020-11-08 16:19:48 +0000

    added ability to set and get values via the mouse

--- Life Sim/Assets/Scripts/GridMap.cs
+++ Life Sim/Assets/Scripts/GridMap.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 
@@ -16,7 +17,7 @@ public class GridMap
     //the map array
     private int[,] m_gridarray;
 
-    Color red = new Color(255, 0, 0);
+    Color green = new Color(0, 255, 0);
 
     //constructor for a new map
     public GridMap(int t_x, int t_y , float t_cellsize)
@@ -32,24 +33,73 @@ public class GridMap
             for(int y = 0; y < m_gridarray.GetLength(1); y++)
             {
 
-                Debug.DrawLine(worldPos(x, y), worldPos(x , y + 1) , red , 100f);
+                Debug.DrawLine(worldPos(x, y), worldPos(x , y + 1) , green , 100f);
 
-                Debug.DrawLine(worldPos(x, y), worldPos(x + 1, y), red, 100f);
+                Debug.DrawLine(worldPos(x, y), worldPos(x + 1, y), green, 100f);
 
             }
         }
 
-        Debug.DrawLine(worldPos(m_width, 0), worldPos(m_width , m_height), red, 100f);
+        Debug.DrawLine(worldPos(m_width, 0), worldPos(m_width , m_height), green, 100f);
 
-        Debug.DrawLine(worldPos(0 ,m_height), worldPos(m_width, m_height), red, 100f);
+        Debug.DrawLine(worldPos(0 ,m_height), worldPos(m_width, m_height), green, 100f);
     }
 
 
 
-    //returns the position of a cell in the array
+    //returns the position of a cell in the world for the array
     private Vector3 worldPos(int t_x, int t_y)
     {
         return (new Vector3(t_x, t_y , 0) * m_cellSize);
     }
 
+
+    //returns x,y depending on the world pos
+    private void getXY(Vector3 t_position , out int t_x , out int t_y)
+    {
+        t_x = Mathf.FloorToInt(t_position.x / m_cellSize);
+        t_y = Mathf.FloorToInt(t_position.y / m_cellSize);
+    }
+
+    private void setValue(int t_x, int t_y , int t_value)
+    {
+        if (t_x >= 0 && t_y >= 0 && t_x < m_width && t_y < m_height)
+        {
+            m_gridarray[t_x, t_y] = t_value;
+        }
+    }
+
+    public void setValue(Vector3 t_pos, int t_value)
+    {
+        int x;
+        int y;
+
+        getXY(t_pos, out x, out y);
+
+        setValue(x, y, t_value);
+    }
+
+    public int getValue(int t_x, int t_y)
+    {
+        if (t_x >= 0 && t_y >= 0 && t_x < m_width && t_y < m_height)
+        {
+            return m_gridarray[t_x, t_y];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public int getValue(Vector3 t_pos)
+    {
+        int x;
+        int y;
+
+        getXY(t_pos, out x, out y);
+
+        Debug.Log(x + " " + y);
+
+        return m_gridarray[x, y];
+    }
 }
--- Life Sim/Assets/Scripts/Map.cs
+++ Life Sim/Assets/Scripts/Map.cs
@@ -1,14 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class Map : MonoBehaviour
 {
+
+    private GridMap m_gridMap;
     // Start is called before the first frame update
     void Start()
     {
-        GridMap gridmap = new GridMap(1, 1, 10f);
+         m_gridMap = new GridMap(10, 10, 10f);
     }
 
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+           
+            vec.z = 0f;
+
+            Debug.Log(vec);
+
+            m_gridMap.setValue(vec, 10);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            vec.z = 0f;
+
+            Debug.Log(vec);
+
+            Debug.Log(m_gridMap.getValue(vec));
+        }
+    }
 
 }
